7f0ca554ba634bfb3e4aaac089fa5cb93290a4c7
serjvaleev <Gabryelf@yandex.ru>
2024-01-15 22:38:51 +0300

    Init employee_reflection_API .

---
+++ src/main/java/org/example/sem_2/Reflection_API/employee_reflection/NotNull.java
@@ -0,0 +1,12 @@
+package org.example.sem_2.Reflection_API.employee_reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface NotNull {
+    String message() default "Field cannot be null";
+}
---
+++ src/main/java/org/example/sem_2/Reflection_API/employee_reflection/Range.java
@@ -0,0 +1,14 @@
+package org.example.sem_2.Reflection_API.employee_reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Range {
+    int min() default Integer.MIN_VALUE;
+    int max() default Integer.MAX_VALUE;
+    String message() default "Field value is out of range";
+}
